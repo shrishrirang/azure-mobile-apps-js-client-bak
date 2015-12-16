b6749e83b8722b9f5277158b7174b58622d21434
Shrirang Shirodkar <shrirs@microsoft.com>
2015-12-16 12:38:22 -0800

    Fix authentication scenarios (and corresponding tests) - Authentication using WebAuthBroker falls back to use the authenticateAsync method if authenticateAndContinue exists but is not implemented - Request only auth token while performing authentication. Default is to send auth token as well as the auth cookie. - Fixed miscellaneous issues in SSO code paths - Removed stale table names and permissions from e2e tests - Property 'Identities' is serialized by the server using camerl casing convention. Fix the case in the client app accordingly. - Access token is sent down to the client as 'access_token'. Make corresponding changes to the tests. - Fixed Live SDK test for Cordova/Win* - Fixed implementation of alert() for Cordova/Windows. window.alert isn't available on Cordova/Windows, use appropriate Win* implementation instead.
    - Remove stale table names and permissions
    - Property 'Identities' is serialized using camel case convention. Fix the case.
    - Access token is sent down to the client as 'access_token'. Make corresponding changes to the test.

--- e2etest/cordova/www/js/platformSpecificFunctions.js
+++ e2etest/cordova/www/js/platformSpecificFunctions.js
@@ -5,13 +5,23 @@
 function createPlatformSpecificFunctions() {
 
     var alertFunction;
-
-    alertFunction = function (text, done) {
-        window.alert(text);
-        if (done) {
-            done();
+    if (typeof alert === 'undefined') {
+        alertFunction = function (text, done) {
+            var dialog = new Windows.UI.Popups.MessageDialog(text);
+            dialog.showAsync().done(function () {
+                if (typeof done === 'function') {
+                    done();
+                }
+            });
         }
-    };
+    } else {
+        alertFunction = function (text, done) {
+            window.alert(text);
+            if (done) {
+                done();
+            }
+        };
+    }
 
     var saveAppInfo = function (lastAppUrl) {
         /// <param name="lastAppUrl" type="String">The last value used in the application URL text box</param>
