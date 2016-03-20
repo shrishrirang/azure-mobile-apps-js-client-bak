a175d35299105276417be70d0d23084bc3d9a010
Shrirang Shirodkar <shrirs@microsoft.com>
2016-03-20 16:18:24 -0700

    Temporarily disable AAD test
    - Re-enable this after the e2e test backend has support for AAD auth.

--- e2etest/shared/tests/loginTests.js
+++ e2etest/shared/tests/loginTests.js
@@ -39,7 +39,7 @@ function defineLoginTestsNamespace() {
 
     var lastUserIdentityObject = null;
 
-    var providers = ['facebook', 'google', 'twitter', 'microsoftaccount', 'aad'];
+    var providers = ['facebook', 'google', 'twitter', 'microsoftaccount'];//, 'aad'];
     for (i = 0; i < providers.length; i++) {
         var provider = providers[i];
         tests.push(createLogoutTest());
