65aa9e07015fdf5669572d9a587758d418d15e4e
Shrirang Shirodkar <shrirs@microsoft.com>
2016-04-22 16:16:43 -0700

    Point the 'Platforms' alias to the cordova directory
    - While building Cordova bundles, the Platforms alias now points to the platforms/cordova directory. This is needed as we will be adding Cordova specific modules.
    - platforms/cordova/platforms.js simply reuses the browser implementation.
    
    - Corrected the list of files for the cordovaTest target.

---
+++ sdk/src/Platforms/cordova/Platform.js
@@ -0,0 +1,6 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+// Just use the browser implementation of Platform specific APIs
+module.exports = require('../web/Platform');
