32808cca4c3871bf21263f740036ab0d4335e31c
Shrirang Shirodkar <shrirs@microsoft.com>
2016-04-22 17:00:24 -0700

    Add SQLite serializer / deserializer
    - SQLiteSerializer.serialize(..) converts the JS value into a form compatible with the SQLite table definition.
    - SQLiteSerializer.deserialize(..) converts the value read from a SQLite table into a JS object as per the specified table definition.

---
+++ sdk/src/Platforms/cordova/SQLiteTypes.js
@@ -0,0 +1,19 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+exports.ColumnType = {
+    Object: "object",
+    Array: "array",
+    Integer: "integer",
+    Int: "int",
+    Float: "float",
+    Real: "real",
+    String: "string",
+    Text: "text",
+    Boolean: "boolean",
+    Bool: "bool",
+    Date: "date"
+};
+
+//FIXME: Numeric data type
