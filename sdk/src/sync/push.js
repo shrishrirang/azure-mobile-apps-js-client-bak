9f74192b99147dc3dd144f4df4dcbb8ae8336167
Shrirang Shirodkar <shrirs@microsoft.com>
2016-06-02 15:54:38 -0700

    Push and pull implementation
    - Incremental pull not yet implemented

---
+++ sdk/src/sync/push.js
@@ -0,0 +1,183 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+/**
+ * Table push logic implementation
+ */
+
+var Validate = require('../Utilities/Validate'),
+    Query = require('query.js').Query,
+    verror = require('verror'),
+    Platform = require('Platforms/Platform'),
+    taskRunner = require('../Utilities/taskRunner'),
+    MobileServiceTable = require('../MobileServiceTable'),
+    tableConstants = require('../constants').table,
+    sysProps = require('../constants').table.sysProps,
+    createPushError = require('./pushError').createPushError,
+    handlePushError = require('./pushError').handlePushError,
+    _ = require('../Utilities/Extensions');
+
+function createPushManager(client, store, storeTaskRunner, operationTableManager) {
+    // Task runner for running push tasks. We want only one push to run at a time. 
+    var pushTaskRunner = taskRunner(),
+        lastProcessedOperationId,
+        pushErrors,
+        pushHandler;
+    
+    return {
+        push: push
+    };
+
+    /**
+     * Pushes operations performed on the local store to the server tables.
+     * 
+     * @param pushHandler An optional object for handling push errors like conflicts, network error, etc
+     *                    
+     *        pushHandler.onRecordPushError(error, tableName, action, data) - this is called when an error is encountered while pushing
+     *                    a record to the server. 
+     *                    error - Error encountered while pushing the record to the server
+     *                    tableName - name of the table for which the push was being performed 
+     *                    action - action that was being pushed. Valid actions are: 'insert', 'update' and 'delete'.
+     *                    data - The value of the record that was pushed. data will be undefined if action is 'delete'
+     *
+     * TODO: Add detailed documentation
+     * 
+     * @returns A promise that is fulfilled when all pending operations are pushed. Conflict errors won't fail the push operation.
+     *          All conflicts are collected and returned to the user at the completion of the push operation. 
+     *          The promise is rejected if the push fails or is cancelled.
+     */
+    function push(handler) {
+        return pushTaskRunner.run(function() {
+            reset();
+            pushHandler = handler;
+            return pushAllOperations().then(function() {
+                return pushErrors;
+            });
+        });
+    }
+    
+    // Resets the state for starting a new push operation
+    function reset() {
+        lastProcessedOperationId = -1; // Initialize to an invalid operation id
+        pushErrors = [];
+    }
+    
+    // Pushes all pending operations, one at a time.
+    // 1. Read the oldest pending operation
+    // 2. Lock the operation obtained in step 1. If 1 did not fetch any operation, we are done push is complete.
+    // 3. Push the operation obtained in step 1.
+    // 4. If 3 is successful, remove the locked operation from the operation table. Else FIXME: TBD
+    // 5. Go to 1. 
+    function pushAllOperations() {
+        var currentOperation,
+            pushError;
+        return readAndLockFirstPendingOperation().then(function(pendingOperation) {
+            if (!pendingOperation) {
+                return; // No more pending operations. Push is complete
+            }
+            
+            var currentOperation = pendingOperation;
+            
+            return pushOperation(currentOperation).then(function() {
+                return removeLockedOperation();
+            }, function(error) {
+                // failed to push
+                // FIXME: Handle errors / conflicts
+                return unlockPendingOperation().then(function() {
+                    pushError = createPushError(store, storeTaskRunner, currentOperation, error);
+                    return handlePushError(pushError, pushHandler);
+                });
+            }).then(function() {
+                if (!pushError) { // no push error
+                    lastProcessedOperationId = currentOperation.logRecord.id;
+                } else if (pushError && !pushError.isHandled) { // push failed and not handled
+
+                    // For conflict errors, we add the error to the list of errors and continue pushing other records
+                    // For other errors, we fail push
+                    if (pushError.isConflict()) {
+                        lastProcessedOperationId = currentOperation.logRecord.id;
+                        pushErrors.push(pushError.getError());
+                    } else { 
+                        throw new verror.VError(pushError.getError(), 'Push failed while pushing operation for tableName : ' + currentOperation.logRecord.tableName +
+                                                                 ', action: ' + currentOperation.logRecord.action +
+                                                                 ', and record ID: ' + currentOperation.logRecord.itemId);
+                    }
+                } else { // push error handled
+                    // NOP.- We want the operation to be re-pushed. If it has been cancelled, that is fine too. 
+                }
+            }).then(function() {
+                return pushAllOperations(); // push remaining operations
+            });
+        });
+    }
+    
+    function readAndLockFirstPendingOperation() {
+        return storeTaskRunner.run(function() {
+            var pendingOperation;
+            return operationTableManager.readFirstPendingOperationWithData(lastProcessedOperationId).then(function(operation) {
+                pendingOperation = operation;
+                
+                if (!pendingOperation) {
+                    return;
+                }
+                
+                return operationTableManager.lockOperation(pendingOperation.logRecord.id);
+            }).then(function() {
+                return pendingOperation;
+            });
+        });
+    }
+    
+    function unlockPendingOperation() {
+        return storeTaskRunner.run(function() {
+            return operationTableManager.unlockOperation();
+        });
+    }
+    
+    function removeLockedOperation() {
+        return storeTaskRunner.run(function() {
+            return operationTableManager.removeLockedOperation();
+        });
+    }
+    
+    function pushOperation(operation) {
+        
+        return Platform.async(function(callback) {
+            callback();
+        })().then(function() {
+            // TODO: push request filter
+        }).then(function() {
+            // perform push
+
+            var mobileServiceTable = client.getTable(operation.logRecord.tableName);
+            switch(operation.logRecord.action) {
+                case 'insert':
+                    removeSysProps(operation.data); // We need to remove system properties before we insert in the server table
+                    return mobileServiceTable.insert(operation.data).then(function(result) {
+                        store.upsert(operation.logRecord.tableName, result); // Upsert the result of insert into the local table
+                    });
+                case 'update':
+                    return mobileServiceTable.update(operation.data).then(function(result) {
+                        store.upsert(operation.logRecord.tableName, result); // Upsert the result of update into the local table
+                    });
+                case 'delete':
+                    return mobileServiceTable.del({id: operation.logRecord.itemId});
+                default:
+                    throw new Error('Unsupported action ' + operation.logRecord.action);
+            }
+            
+        }).then(function() {
+            // TODO: push response filter
+        });
+        
+    }
+    
+    function removeSysProps(record) {
+        for (var i in sysProps) {
+            delete record[sysProps[i]];
+        }
+    }
+}
+
+exports.createPushManager = createPushManager;
---
+++ sdk/test/tests/target/cordova/offline.functional.tests.js
@@ -0,0 +1,243 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+/**
+ * Functional tests for offline scenarios
+ */
+
+var Platform = require('Platforms/Platform'),
+    Query = require('query.js').Query,
+    pullManager = require('../../../../src/sync/pull'),
+    MobileServiceSyncContext = require('../../../../src/sync/MobileServiceSyncContext'),
+    MobileServiceClient = require('../../../../src/MobileServiceClient'),
+    MobileServiceSqliteStore = require('Platforms/MobileServiceSqliteStore'),
+    uuid = require('node-uuid'),
+    storeTestHelper = require('./storeTestHelper');
+    
+var client = new MobileServiceClient('http://shrirs-js-dev.azurewebsites.net' /* TODO: Make this configurable */),
+    syncContext = new MobileServiceSyncContext(client),
+    testTableName = storeTestHelper.testTableName,
+    table = client.getTable(testTableName),
+    query = new Query(testTableName),
+    serverValue,
+    clientValue,
+    testId,
+    store;
+    
+$testGroup('offline tests')
+    .functional()
+    .beforeEachAsync(function() {
+        return storeTestHelper.createEmptyStore().then(function(localStore) {
+            store = localStore;
+            return store.defineTable({
+                name: testTableName,
+                columnDefinitions: {
+                    id: MobileServiceSqliteStore.ColumnType.String,
+                    text: MobileServiceSqliteStore.ColumnType.String,
+                    complete: MobileServiceSqliteStore.ColumnType.Boolean,
+                    version: MobileServiceSqliteStore.ColumnType.String
+                }
+            });
+        }).then(function() {
+            serverValue = clientValue = undefined;
+            return syncContext.initialize(store);
+        });
+    }).tests(
+
+    $test('Basic push - insert / update / delete')
+    .checkAsync(function () {
+        var actions = [
+            'clientinsert', 'push', 'serverlookup',
+            {
+                success: function(result) {
+                    $assert.isNotNull(clientValue);
+                    $assert.areEqual(result.id, clientValue.id);
+                    $assert.areEqual(result.text, clientValue.text);
+                }
+            },
+            'clientupdate', 'push', 'serverlookup',
+            {
+                success: function(result) {
+                    $assert.isNotNull(clientValue);
+                    $assert.areEqual(result.id, clientValue.id);
+                    $assert.areEqual(result.text, clientValue.text);
+                }
+            },
+            'clientdelete', 'push', 'serverlookup',
+            {
+                success: function(result) {
+                    $assert.fail('should have failed to lookup deleted server record');
+                },
+                fail: function(error) {
+                    // error expected
+                }
+            }
+        ];
+                        
+        return performActions(actions);
+    }),
+    
+    $test('vanilla pull - insert / update / delete')
+    .checkAsync(function () {
+        var actions = [
+            'serverinsert', 'vanillapull', 'clientlookup',
+            {
+                success: function(result) {
+                    $assert.areEqual(result.id, serverValue.id);
+                    $assert.areEqual(result.text, serverValue.text);
+                }
+            },
+            'serverupdate', 'vanillapull', 'clientlookup',
+            {
+                success: function(result) {
+                    $assert.areEqual(result.id, serverValue.id);
+                    $assert.areEqual(result.text, serverValue.text);
+                }
+            },
+            'serverdelete', 'vanillapull', 'clientlookup',
+            {
+                success: function(result) {
+                    $assert.isNull(result);
+                },
+                fail: function(error) {
+                    $assert.fail(error);
+                }
+            }
+        ];
+        
+        return performActions(actions);
+    }),
+    
+    $test('Push conflict - insert')
+    .checkAsync(function () {
+        
+        var count = 0;
+        function onRecordPushError(pushError) {
+            if (pushError.isConflict()) {
+                var newValue = pushError.getClientRecord();
+
+                return table.lookup(newValue.id).then(function(result) {
+                    newValue.version = result.version;
+                    return pushError.changeAction('update');
+                }).then(function() {
+                    pushError.isHandled = true;
+                });
+            }
+        }
+        
+        var actions = [
+            'serverinsert', 'clientinsert',
+            {
+                success: function(result) {
+                    return syncContext.push({
+                        onRecordPushError: onRecordPushError
+                    }).then(function(conflicts) {
+                        $assert.areEqual(conflicts.length, 0);
+                    });
+                }
+            },
+            'serverlookup',
+            {
+                success: function(result) {
+                    $assert.areEqual(result.id, clientValue.id);
+                    $assert.areEqual(result.text, clientValue.text);
+                }
+            }
+        ];
+        
+        return performActions(actions);
+    })
+);
+
+function performActions (actions) {
+    
+    testId = generateId();
+    
+    var chain = Platform.async(function(callback) {
+        callback();
+    })();
+    
+    for (var i in actions) {
+        chain = performAction(chain, actions[i]);
+    }
+    
+    return chain;
+}
+
+function performAction (chain, action) {
+    var record;
+    return chain.then(function(result) {
+        if (action && action.success) {
+            return action.success(result);
+        }
+        
+        switch(action) {
+            case 'clientinsert':
+                record = generateRecord('client-insert');
+                return syncContext.insert(testTableName, record).then(function(result) {
+                    clientValue = result;
+                    return result;
+                });
+            case 'clientupdate':
+                record = generateRecord('client-update')
+                return syncContext.update(testTableName, record).then(function(result) {
+                    clientValue = result;
+                    return result;
+                });
+            case 'clientdelete':
+                record = generateRecord(id)
+                return syncContext.del(testTableName, record).then(function(result) {
+                    clientValue = undefined;
+                    return result;
+                });
+            case 'clientlookup':
+                return syncContext.lookup(testTableName, testId);
+            case 'serverinsert':
+                record = generateRecord('server-insert');
+                return table.insert(record).then(function(result) {
+                    serverValue = result;
+                    return result;
+                });
+            case 'serverupdate':
+                record = generateRecord('server-update');
+                return table.update(record).then(function(result) {
+                    serverValue = result;
+                    return result;
+                });
+            case 'serverdelete':
+                record = generateRecord(id);
+                return table.del(record).then(function(result) {
+                    serverValue = undefined;
+                    return result;
+                });
+            case 'serverlookup':
+                return table.lookup(testId);
+            case 'push':
+                return syncContext.push();
+            case 'vanillapull':
+                return syncContext.pull(query);
+            default:
+                throw new Error('Unsupported action : ' + action);
+        }
+    }, function(error) {
+        if (action && action.fail) {
+            return action.fail(error);
+        } else {
+            $assert.fail('Unexpected failure while running action : ' + action);
+            $assert.fail(error);
+            throw error;
+        }
+    });
+}
+
+function generateRecord(textPrefix) {
+    return {
+        id: testId,
+        text: textPrefix + uuid.v4()
+    }
+}
+
+function generateId() {
+    return uuid.v4();
+}
