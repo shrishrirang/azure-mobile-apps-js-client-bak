e908828416153fa3b0e0855b8f184463162d0638
Shrirang Shirodkar <shrirs@microsoft.com>
2016-06-03 11:33:11 -0700

    Push and pull implementation
    - Incremental pull not yet implemented

---
+++ sdk/src/sync/push.js
@@ -0,0 +1,175 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+/**
+ * Table push logic implementation
+ */
+
+var Validate = require('../Utilities/Validate'),
+    Query = require('query.js').Query,
+    verror = require('verror'),
+    Platform = require('Platforms/Platform'),
+    taskRunner = require('../Utilities/taskRunner'),
+    MobileServiceTable = require('../MobileServiceTable'),
+    tableConstants = require('../constants').table,
+    sysProps = require('../constants').table.sysProps,
+    createPushError = require('./pushError').createPushError,
+    handlePushError = require('./pushError').handlePushError,
+    _ = require('../Utilities/Extensions');
+
+function createPushManager(client, store, storeTaskRunner, operationTableManager) {
+    // Task runner for running push tasks. We want only one push to run at a time. 
+    var pushTaskRunner = taskRunner(),
+        lastProcessedOperationId,
+        pushConflicts,
+        pushHandler;
+    
+    return {
+        push: push
+    };
+
+    /**
+     * Pushes operations performed on the local store to the server tables.
+     * 
+     * @returns A promise that is fulfilled when all pending operations are pushed. Conflict errors won't fail the push operation.
+     *          All conflicts are collected and returned to the user at the completion of the push operation. 
+     *          The promise is rejected if pushing any record fails for reasons other than conflict or is cancelled.
+     */
+    function push(handler) {
+        return pushTaskRunner.run(function() {
+            reset();
+            pushHandler = handler;
+            return pushAllOperations().then(function() {
+                return pushConflicts;
+            });
+        });
+    }
+    
+    // Resets the state for starting a new push operation
+    function reset() {
+        lastProcessedOperationId = -1; // Initialize to an invalid operation id
+        pushConflicts = [];
+    }
+    
+    // Pushes all pending operations, one at a time.
+    // 1. Read the oldest pending operation
+    // 2. If 1 did not fetch any operation, go to 6.
+    // 3. Lock the operation obtained in step 1 and push it.
+    // 4. If 3 is successful, unlock and remove the locked operation from the operation table and go to 1
+    //    Else if 3 fails, unlock the operation.
+    // 5. If the error is a conflict, handle the conflict and go to 1.
+    // 6. Else, EXIT.
+    function pushAllOperations() {
+        var currentOperation,
+            pushError;
+        return readAndLockFirstPendingOperation().then(function(pendingOperation) {
+            if (!pendingOperation) {
+                return; // No more pending operations. Push is complete
+            }
+            
+            var currentOperation = pendingOperation;
+            
+            return pushOperation(currentOperation).then(function() {
+                return removeLockedOperation();
+            }, function(error) {
+                // failed to push
+                // FIXME: Handle errors / conflicts
+                return unlockPendingOperation().then(function() {
+                    pushError = createPushError(store, storeTaskRunner, currentOperation, error);
+                    return handlePushError(pushError, pushHandler);
+                });
+            }).then(function() {
+                if (!pushError) { // no push error
+                    lastProcessedOperationId = currentOperation.logRecord.id;
+                } else if (pushError && !pushError.isHandled) { // push failed and not handled
+
+                    // For conflict errors, we add the error to the list of errors and continue pushing other records
+                    // For other errors, we fail push
+                    if (pushError.isConflict()) {
+                        lastProcessedOperationId = currentOperation.logRecord.id;
+                        pushConflicts.push(pushError.getError());
+                    } else { 
+                        throw new verror.VError(pushError.getError(), 'Push failed while pushing operation for tableName : ' + currentOperation.logRecord.tableName +
+                                                                 ', action: ' + currentOperation.logRecord.action +
+                                                                 ', and record ID: ' + currentOperation.logRecord.itemId);
+                    }
+                } else { // push error handled
+                    // No action needed - We want the operation to be re-pushed.
+                    // No special handling is needed even if the operation was cancelled by the user as part of error handling  
+                }
+            }).then(function() {
+                return pushAllOperations(); // push remaining operations
+            });
+        });
+    }
+    
+    function readAndLockFirstPendingOperation() {
+        return storeTaskRunner.run(function() {
+            var pendingOperation;
+            return operationTableManager.readFirstPendingOperationWithData(lastProcessedOperationId).then(function(operation) {
+                pendingOperation = operation;
+                
+                if (!pendingOperation) {
+                    return;
+                }
+                
+                return operationTableManager.lockOperation(pendingOperation.logRecord.id);
+            }).then(function() {
+                return pendingOperation;
+            });
+        });
+    }
+    
+    function unlockPendingOperation() {
+        return storeTaskRunner.run(function() {
+            return operationTableManager.unlockOperation();
+        });
+    }
+    
+    function removeLockedOperation() {
+        return storeTaskRunner.run(function() {
+            return operationTableManager.removeLockedOperation();
+        });
+    }
+    
+    function pushOperation(operation) {
+        
+        return Platform.async(function(callback) {
+            callback();
+        })().then(function() {
+            // TODO: Invoke push request filter to allow user to change how the record is sent to the server
+        }).then(function() {
+            // perform push
+
+            var mobileServiceTable = client.getTable(operation.logRecord.tableName);
+            switch(operation.logRecord.action) {
+                case 'insert':
+                    removeSysProps(operation.data); // We need to remove system properties before we insert in the server table
+                    return mobileServiceTable.insert(operation.data).then(function(result) {
+                        return store.upsert(operation.logRecord.tableName, result); // Upsert the result of insert into the local table
+                    });
+                case 'update':
+                    return mobileServiceTable.update(operation.data).then(function(result) {
+                        return store.upsert(operation.logRecord.tableName, result); // Upsert the result of update into the local table
+                    });
+                case 'delete':
+                    return mobileServiceTable.del({id: operation.logRecord.itemId});
+                default:
+                    throw new Error('Unsupported action ' + operation.logRecord.action);
+            }
+            
+        }).then(function() {
+            // TODO: Invoke hook to notify record push completed successfully
+        });
+        
+    }
+    
+    function removeSysProps(record) {
+        for (var i in sysProps) {
+            delete record[sysProps[i]];
+        }
+    }
+}
+
+exports.createPushManager = createPushManager;
---
+++ sdk/src/sync/pushError.js
@@ -0,0 +1,311 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+/**
+ * @file Table push error handling implementation. Defines various methods for resolving conflicts
+ */
+
+var Platform = require('Platforms/Platform'),
+    _ = require('../Utilities/Extensions'),
+    tableConstants = require('../constants').table;
+    
+var operationTableName = tableConstants.operationTableName,
+    deletedColumnName = tableConstants.sysProps.deletedColumnName;
+
+/**
+ * Creates a pushError object that wraps the low level error encountered while pushing
+ * and adds other useful methods for error handling.
+ */
+function createPushError(store, storeTaskRunner, pushOperation, operationError) {
+    
+    return {
+        isHandled: false,
+        getError: getError,
+        
+        // Helper methods
+        isConflict: isConflict,
+        
+        // Data query methods
+        getTableName: getTableName,
+        getAction: getAction,
+        getServerRecord: getServerRecord,
+        getClientRecord: getClientRecord,
+        
+        // Error handling methods
+        cancelAndUpdate: cancelAndUpdate,
+        cancelAndDiscard: cancelAndDiscard,
+        cancel: cancel,
+        update: update,
+        changeAction: changeAction
+    };
+    
+    /**
+     * Get the name of the table for which push was performed
+     */
+    function getTableName() {
+        return makeCopy(pushOperation.logRecord.tableName);
+    }
+    
+    /**
+     * Gets the action that was pushed to the server.
+     * Action can be one of 'insert', 'update' or 'delete'.
+     */
+    function getAction() {
+        return makeCopy(pushOperation.logRecord.action);
+    }
+    
+    /**
+     * Gets the value of the server record, if available.
+     * Value of the server record may not be available always.
+     * Example: If the push failed due to a connection error, the value of server
+     * record won't be available.
+     */
+    function getServerRecord() {
+        return makeCopy(operationError.serverInstance);
+    }
+    
+    /**
+     * Gets the value of the client record that was sent to the server.
+     * Note that this may not be the latest value.
+     */
+    function getClientRecord() {
+        return makeCopy(pushOperation.data);
+    }
+    
+    /**
+     * Gets the underlying error.
+     * This contains grannular details about the failure. Egs: server response, etc
+     */
+    function getError() {
+        return makeCopy(operationError);
+    }
+    
+    /**
+     * Checks if the current error is a conflict error.
+     * @returns true - if the current error is a conflict error. false - otherwise.
+     */
+    function isConflict() {
+        return operationError.request.status === 409 || operationError.request.status === 412;
+    }
+    
+    /**
+     * Cancels the push operation for the current record and updates the record in the local store.
+     * 
+     * @param newValue New value of the client record that will be updated in the local store.
+     * 
+     * @returns A promise that is fulfilled when the operation is cancelled and the client record is updated.
+     */
+    function cancelAndUpdate(newValue) {
+        return storeTaskRunner.run(function() {
+
+            if (pushOperation.logRecord.action === 'delete') {
+                throw new Error('Cannot update a deleted record');
+            }
+            
+            if (_.isNull(newValue)) {
+                throw new Error('Need a valid object to update the record');
+            }
+            
+            if (!_.isValidId(newValue.id)) {
+                throw new Error('Invalid ID: ' + newValue.id);
+            }
+            
+            if (newValue.id !== pushOperation.data.id) {
+                throw new Error('Only updating the record being pushed is allowed');
+            }
+            
+            // Operation to update the data record
+            var dataUpdateOperation = {
+                tableName: pushOperation.logRecord.tableName,
+                action: 'upsert',
+                data: newValue
+            };
+            
+            // Operation to delete the log record
+            var logDeleteOperation = {
+                tableName: operationTableName,
+                action: 'delete',
+                data: newValue.id
+            }
+            
+            // Execute the log and data operations
+            var operations = cancelRecordPush ? [dataUpdateOperation, logDeleteOperation] : [dataUpdateOperation];
+            return store.executeBatch(operations);
+        });
+    }
+    
+    /**
+     * Cancels the push operation for the current record and discards the record from the local store.
+     * 
+     * @returns A promise that is fulfilled when the operation is cancelled and the client record is discarded.
+     */
+    function cancelAndDiscard() {
+        return storeTaskRunner.run(function() {
+            
+            // Operation to delete the data record
+            var dataDeleteOperation = {
+                tableName: pushOperation.logRecord.tableName,
+                action: 'delete',
+                data: pushOperation.logRecord.itemId
+            };
+            
+            // Operation to delete the log record
+            var logDeleteOperation = {
+                tableName: operationTableName,
+                action: 'delete',
+                data: pushOperation.logRecord.id
+            }
+            
+            // Execute the log and data operations
+            var operations = cancelRecordPush ? [dataDeleteOperation, logDeleteOperation] : [dataDeleteOperation];
+            return store.executeBatch(operations);
+        });
+    }
+    
+    /**
+     * Updates the client data record associated with the current operation.
+     *
+     * @param newValue New value of the data record. 
+     * 
+     * @returns A promise that is fulfilled when the data record is updated in the localstore.
+     */
+    function update(newValue) {
+        return storeTaskRunner.run(function() {
+            if (pushOperation.logRecord.action === 'delete') {
+                throw new Error('Cannot update a deleted record');
+            }
+            
+            if (_.isNull(newValue)) {
+                throw new Error('Need a valid object to update the record');
+            }
+            
+            if (!_.isValidId(newValue.id)) {
+                throw new Error('Invalid ID: ' + newValue.id);
+            }
+            
+            if (newValue.id !== pushOperation.data.id) {
+                throw new Error('Only updating the record being pushed is allowed');
+            }
+
+            //TODO: Do we need to disallow updating record if the record has been deleted after
+            //we attempted push?
+                        
+            return store.upsert(pushOperation.logRecord.tableName, newValue);
+        });
+    }
+    
+    /**
+     * Changes the type of operation that will be pushed to the server.
+     * This is useful for handling conflicts where you might need to change the type of the 
+     * operation to be able to push the changes to the server.
+     *
+     * Example: You might need to change 'insert' to 'update' to be able to push a record that 
+     * was already inserted on the server.
+     * 
+     * Note: Changing the action to delete will automatically remove the associated record from the 
+     * data table in the local store.
+     * 
+     * @param newAction New type of the operation. Valid values are 'insert', 'update' and 'delete'
+     * @param [newClientRecord] New value of the client record. The new record ID should match the original record ID. Also,
+     *                         a new record value cannot be specified if the new action is 'delete'
+     * 
+     * @returns A promise that is fulfilled when the action is changed and, optionally, the data record is updated / deleted.
+     */
+    function changeAction(newAction, newClientRecord) {
+        return storeTaskRunner.run(function() {
+            var dataOperation, // operation to edit the data record
+                logOperation = { // operation to edit the log record 
+                    tableName: operationTableName,
+                    action: 'upsert',
+                    data: makeCopy(pushOperation.logRecord)
+                };
+            
+            if (newAction === 'insert' || newAction === 'update') {
+                
+                // Change the action as specified
+                logOperation.data.action = newAction;
+                
+                // Update the client record, if a new value is specified
+                if (newClientRecord) {
+                    
+                    if (!newClientRecord.id) {
+                        throw new Error('New client record value must specify the record ID');
+                    }
+                    
+                    if (newClientRecord.id !== pushOperation.logRecord.itemId) {
+                        throw new Error('New client record value cannot change the record ID. Original ID: ' + pushOperation.logRecord.id + ' New ID: ' + newClientRecord.id);
+                    }
+                    
+                    dataOperation = {
+                        tableName: pushOperation.logRecord.tableName,
+                        action: 'upsert',
+                        data: newClientRecord
+                    };
+                    
+                }
+                
+            } else if (newAction === 'delete' || newAction === 'del') {
+
+                if (newClientRecord) {
+                    throw new Error('Cannot specify a new value for the client record if the new action is delete');
+                }
+
+                // Change the action to 'delete'
+                logOperation.data.action = 'delete';
+                
+                // Delete the client record as the new action is 'delete'
+                dataOperation = {
+                    tableName: pushOperation.logRecord.tableName,
+                    action: 'delete',
+                    data: pushOperation.logRecord.id
+                };
+
+            } else {
+                throw new Error('Action ' + newAction + ' not supported.');
+            }
+            
+            // Execute the log and data operations
+            var operations = dataOperation ? [logOperation, dataOperation] : [logOperation];
+            return store.executeBatch(operations);
+        });
+    }
+    
+    /**
+     * Cancels pushing the current operation to the server permanently.
+     * 
+     * This method simply removes the pending operation from the operation table, thereby 
+     * permanently skipping the associated change. A future change done to the same record
+     * will not be affected and such changes will continue to be pushed. 
+     */
+    function cancel() {
+        return storeTaskRunner.run(function() {
+            return store.del(operationTableName, pushOperation.logRecord.id);
+        });
+    }
+}
+
+function makeCopy(value) {
+    if (!_.isNull(value)) {
+        value = JSON.parse( JSON.stringify(value) );
+    }
+    return value;
+}
+
+/**
+ * Attempts error handling by delegating it to the user, if a push handler is provided
+ */
+function handlePushError(pushError, pushHandler) {
+    return Platform.async(function(callback) {
+        callback();
+    })().then(function() {
+        
+        // Check if a handler is provided for errors encountered while pushing records
+        if (pushHandler && pushHandler.onRecordPushError) {
+            return pushHandler.onRecordPushError(pushError);
+        }
+    });
+}
+
+exports.createPushError = createPushError;
+exports.handlePushError = handlePushError;
