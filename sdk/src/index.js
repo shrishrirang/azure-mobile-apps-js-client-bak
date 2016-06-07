4a00e15c739e7ea0ca63c4cb0117b07e3fadbdff
Shrirang Shirodkar <shrirs@microsoft.com>
2016-06-07 13:42:48 -0700

    Add few modules to the SDK exports

--- sdk/src/index.js
+++ sdk/src/index.js
@@ -2,9 +2,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
 
-// These exports serve as the JS bundle exports
-module.exports = {
+var _ = require('./Utilities/Extensions');
+
+// Modules that need to be exposed outside the SDK for all targets 
+var api = {
     MobileServiceClient: require('./MobileServiceClient'),
     MobileServiceLogin: require('./MobileServiceLogin'),
-    MobileServiceTable: require('./MobileServiceTable')
+    MobileServiceSyncTable: require('./sync/MobileServiceSyncTable'),
+    MobileServiceTable: require('./MobileServiceTable'),
+    Query: require('query.js').Query
 };
+
+// Target (i.e. Cordova / Browser / etc) specific definitions that need to be exposed outside the SDK
+var targetExports = require('Platforms/index');
+
+// Export shared as well as target specific APIs
+for (var i in targetExports) {
+    if ( _.isNull(api[i]) ) {
+        api[i] = targetExports[i];
+    } else {
+        throw new Error('Cannot export definition ' + i + ' outside the SDK. Multiple definitions with the same name exist');
+    }
+}
+
+module.exports = api;
